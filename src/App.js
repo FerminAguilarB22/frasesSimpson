00157eaeb29ec8fe840c06f1b1bb1edc8fb8039f
Fermin Aguilar <ferminab99@gmail.com>
2021-11-11 18:31:53 -0300

    Se realizo la estructura del proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import logo from './logoSimpson.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'react-bootstrap';
+import Frase from './components/Frase';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<section className='container my-5 d-flex flex-column align-items-center'>
+  <img src={logo} alt="Logo de los Simpsons" width='100%' />
+  <Button variant='warning' className='w-50 my-4'>Obtener frase</Button>
+  <Frase></Frase>
+</section>
   );
 }
 
